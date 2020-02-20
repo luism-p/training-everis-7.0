4451a279b625c549f3ab8e089d70e07139cbcaf1
Luis <lm.perezpacheco@gmail.com>
2020-02-20 22:01:58 +0100

    test

--- modules/customers-web/src/main/java/com/liferay/training/customers/actionCommand/CustomerActionCommandRemove.java
+++ modules/customers-web/src/main/java/com/liferay/training/customers/actionCommand/CustomerActionCommandRemove.java
@@ -3,10 +3,7 @@ package com.liferay.training.customers.actionCommand;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.servlet.SessionMessages;
-import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.trainin.customers.model.Customer;
 import com.liferay.trainin.customers.service.CustomerLocalService;
 import com.liferay.training.customers.constants.CustomersConstans;
 import org.osgi.service.component.annotations.Component;
@@ -15,9 +12,6 @@ import org.osgi.service.component.annotations.Reference;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
 
 @Component(
         immediate = true,
